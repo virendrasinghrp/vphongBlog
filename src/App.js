cdc67db9e99eb3f0ba36edde6e8750c03a4e0018
vphong <vphong@ucsd.edu>
2017-10-01 08:56:50 -0700

    cname, cleanup

--- src/App.js
+++ src/App.js
@@ -3,33 +3,36 @@ import './App.css';
 import FontAwesome from 'react-fontawesome'
 
 class App extends Component {
+//
+// constructor() {
+//   super();
+//   this.state = { "avatar_url": ""};
+// }
 
-constructor() {
-  super();
-  this.state = { "avatar_url": ""};
-}
-
-componentDidMount() {
-  var that = this;
-  fetch("https://api.github.com/users/vphong/repos")
-    .then(function(result) {
-      return result.json()
-    }).then(function(json) {
-      console.log(json)
-      that.setState({"avatar_url": json[0].owner.avatar_url})
-    }).catch(function(ex) {
-      console.log('parsing failed', ex)
-    })
-}
+// componentDidMount() {
+  // var that = this;
+  // fetch("https://api.github.com/users/vphong/repos")
+  //   .then(function(result) {
+  //     return result.json()
+  //   }).then(function(json) {
+  //     console.log(json)
+  //     that.setState({"avatar_url": json[0].owner.avatar_url})
+  //   }).catch(function(ex) {
+  //     console.log('parsing failed', ex)
+  //   })
+// }
 
   render() {
     return (
-      <div className="App container">
-        <header className="App-header">
-          <h1 className="App-title">Vanna Phong</h1>
-        </header>
-        <div className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
+      <div className="container">
+        <div className="App">
+          <header className="App-header">
+            <h1 className="App-title">Vanna Phong</h1>
+          </header>
+        </div>
+
+        <div className="icons">
+        <a href="mailto:contact@vanna.io"><FontAwesome name="envelope" /></a>
         </div>
       </div>
     );
